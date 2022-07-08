85a524f1ff81c70e1a6eefe86d80d1f0554aad9f
Cristian S <christian.bx100pre@gmail.com>
2022-07-08 11:56:01 -0500

    comentario de planes de desarrollo y otros

---
+++ main.js
@@ -0,0 +1,11 @@
+//aun no tenemos los conocimientos necesario para crear una app movil 
+//este semestre tenemos planeado terminar con lo siguiente:
+//*App Web (sistema web)
+//*Pagina web
+//*web api
+//el otro semestre tenemos planeado realizar la aplicacion movil y
+//la IA (chat boot)
+
+let Empresa="L&B";
+
+console.log('bienvenidos a la App',Empresa,'Admin');
